2dd91411ec6a177962582bd0fdc7acc496710219
PaulDotSH <84643540+PaulDotSH@users.noreply.github.com>
2021-05-23 17:03:41 +0000

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace decrypter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DecryptFilesButton = new System.Windows.Forms.Button();
+            this.decryptedFilesLabel = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(114, 251);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(391, 29);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(11, 256);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Password:";
+            // 
+            // DecryptFilesButton
+            // 
+            this.DecryptFilesButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DecryptFilesButton.Location = new System.Drawing.Point(15, 308);
+            this.DecryptFilesButton.Name = "DecryptFilesButton";
+            this.DecryptFilesButton.Size = new System.Drawing.Size(492, 43);
+            this.DecryptFilesButton.TabIndex = 2;
+            this.DecryptFilesButton.Text = "Decrypt My Files";
+            this.DecryptFilesButton.UseVisualStyleBackColor = true;
+            this.DecryptFilesButton.Click += new System.EventHandler(this.DecryptFilesButton_Click);
+            // 
+            // decryptedFilesLabel
+            // 
+            this.decryptedFilesLabel.AutoSize = true;
+            this.decryptedFilesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.decryptedFilesLabel.ForeColor = System.Drawing.Color.Red;
+            this.decryptedFilesLabel.Location = new System.Drawing.Point(12, 280);
+            this.decryptedFilesLabel.Name = "decryptedFilesLabel";
+            this.decryptedFilesLabel.Size = new System.Drawing.Size(173, 25);
+            this.decryptedFilesLabel.TabIndex = 6;
+            this.decryptedFilesLabel.Text = "No files decrypted!";
+            this.decryptedFilesLabel.Visible = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(490, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(519, 363);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.decryptedFilesLabel);
+            this.Controls.Add(this.DecryptFilesButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Decryptor";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button DecryptFilesButton;
+        private System.Windows.Forms.Label decryptedFilesLabel;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
